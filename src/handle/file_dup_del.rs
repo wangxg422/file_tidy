6d6fcf24262f1974f02e34efb7eb912591889a92
wangxiguang <wangxiguang1024@outlook.com>
2025-08-07 17:59:50 +0800

    change dup file delete

--- src/handle/file_dup_del.rs
+++ src/handle/file_dup_del.rs
@@ -1,61 +1,82 @@
 use crate::command::file::DupDelArgs;
 use crate::enumerate::file::FileHashType;
+use crate::error::Error;
 use crate::util::hash::compute_file_hash;
+use log::{error, info, warn};
+use rayon::iter::IntoParallelRefIterator;
+use rayon::prelude::*;
 use std::collections::HashMap;
 use std::path::{Path, PathBuf};
-use log::{info, warn};
+use std::sync::{Arc, Mutex};
 use walkdir::WalkDir;
-use crate::error::Error;
 
 pub fn handle(args: &DupDelArgs) -> Result<(), Error> {
-    let mut hashes: HashMap<Vec<u8>, PathBuf> = HashMap::new();
+    let hashes: Arc<Mutex<HashMap<Vec<u8>, PathBuf>>> = Arc::new(Mutex::new(HashMap::new()));
 
-    for entry in WalkDir::new(&args.dir) {
-        let entry = entry.unwrap();
-        let path = entry.path();
+    let entries: Vec<_> = WalkDir::new(&args.dir)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| e.file_type().is_file() && !e.file_name().to_string_lossy().starts_with("."))
+        .map(|e| e.path().to_path_buf())
+        .collect();
 
-        if path.file_name().unwrap().to_string_lossy().starts_with(".") {
-            continue;
-        }
-
-        if path.is_file() {
-            let hash = compute_file_hash(&FileHashType::SHA3, path).unwrap();
-
-            if hashes.contains_key(&hash) {
-                delete_file(hashes.get(&hash).unwrap(), &path, &hash);
-            } else {
-                hashes.insert(hash, path.to_path_buf());
+    entries
+        .par_iter()
+        .for_each(|path| match compute_file_hash(&FileHashType::SHA3, path) {
+            Ok(hash) => {
+                if let Ok(mut map) = hashes.lock() {
+                    if map.contains_key(&hash) {
+                        let file = map.get(&hash).unwrap();
+                        info!(
+                            "File {:?}(SHA3-256: {}) exist, delete it",
+                            file.display(),
+                            hex::encode(hash),
+                        );
+                        delete_file(file, &path);
+                    } else {
+                        map.insert(hash, path.to_path_buf());
+                    }
+                } else {
+                    error!("Failed to acquire lock when processing: {}", path.display());
+                }
             }
-        }
-    }
+            Err(err) => {
+                error!("Failed to compute hash for {}: {}", path.display(), err);
+            }
+        });
 
-    info!("duplicates files clear finished");
-    
+    info!("duplicates files delete finished");
+
     Ok(())
 }
 
-// delete file when sha3-256 and md5 is same
-fn delete_file(exist: &Path, to_delete: &Path, hash_sha3: &Vec<u8>) {
+// delete file if sha3-256 and md5 is same
+fn delete_file(exist: &Path, to_delete: &Path) -> Result<(), Error> {
     let hash_md5_exist = compute_file_hash(&FileHashType::MD5, to_delete).unwrap();
     let hash_md5_delete = compute_file_hash(&FileHashType::MD5, exist).unwrap();
 
     if hash_md5_exist == hash_md5_delete {
         match std::fs::remove_file(to_delete) {
-            Ok(_) => println!("File {} deleted", to_delete.display()),
-            Err(error) => println!("Error delete file {:?}:{:?}", to_delete, error),
+            Ok(_) => {
+                info!("File {} deleted", to_delete.display());
+                Ok(())
+            }
+            Err(err) => {
+                error!("Error delete file {:?}:{:?}", to_delete, err);
+                Err(Error::CustomError(format!("Failed to delete file {:?}: {}", to_delete, err)))
+            }
         }
     } else {
         warn!(
             "Files:\n\
-    - {:?} (SHA3-256: {}, MD5: {})\n\
-    - {:?} (SHA3-256: {}, MD5: {})\n\
+    - {:?} (MD5: {})\n\
+    - {:?} (MD5: {})\n\
     are the same in SHA3-256, but different in MD5.",
             exist,
-            hex::encode(hash_sha3),
             hex::encode(hash_md5_exist),
             to_delete,
-            hex::encode(hash_sha3),
             hex::encode(hash_md5_delete)
         );
+        Err(Error::CustomError(format!("File {} and {} are the same in SHA3-256, but different in MD5", exist.display(), to_delete.display())))
     }
 }
